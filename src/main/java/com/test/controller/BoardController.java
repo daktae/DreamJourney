a2d566043e231dbcb366d698b116b30e1ba98097
DongDongEEEE <cnsdh78@gmail.com>
2023-07-10 16:31:51 +0900

    동재 연우 소엽 merge 2023-07-10 16:31

--- src/main/java/com/test/controller/BoardController.java
+++ src/main/java/com/test/controller/BoardController.java
@@ -2,6 +2,7 @@ package com.test.controller;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.test.domain.BoardDTO;
 import com.test.domain.CommentDTO;
 import com.test.service.BoardService;
+
 @Controller
 public class BoardController {
 	
